20c4640e13b453549a41e8d202725ccf64dc7c21
Brian Fountain <brian@brianfountain.com>
2020-02-04 20:00:15 -0500

    Add question reset

--- main.js
+++ main.js
@@ -4,13 +4,19 @@ const data = [
     choices: [
       {
         body: 'var myFavoriteDay = ["Monday", "Saturday", "Friday"];',
-        correct: true
+        correct: true,
+        code: true
       },
       {
         body: 'var myFavoriteFruits = ["banana", "cherry", "mango",];',
-        correct: false
+        correct: false,
+        code: true
       },
-      { body: 'var myFavoriteThings = ["banana", true, 35,];', correct: false }
+      {
+        body: 'var myFavoriteThings = ["banana", true, 35,];',
+        correct: false,
+        code: true
+      }
     ],
     type: "single-choice"
   },
@@ -25,14 +31,17 @@ const data = [
   }
 ];
 
-var urlParams = new URLSearchParams(window.location.search);
-var questionPresent = urlParams.has("q");
-var questionNumber = parseInt(urlParams.get("q"));
-if (questionPresent && questionNumber < data.length) {
-  question = urlParams.get("q");
-} else {
-  console.log("invalid question. defaulting to first question");
-  question = 0;
+// extract the question from the url
+function questionNumber() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var questionPresent = urlParams.has("q");
+  var questionNumber = parseInt(urlParams.get("q"));
+  if (questionPresent && questionNumber < data.length) {
+    return (questionNumber = urlParams.get("q"));
+  } else {
+    console.log("invalid question. defaulting to first question");
+    return (questionNumber = 0);
+  }
 }
 
 function showQuestion(index, data) {
@@ -40,15 +49,21 @@ function showQuestion(index, data) {
   data[index].choices.forEach((choice, index) => {
     // create label
     let label = document.createElement("label");
-    let span = document.createElement("span");
-    span.innerHTML = " " + choice.body;
+    let wrapper;
+    if (choice.code) {
+      wrapper = document.createElement("code");
+    } else {
+      wrapper = document.createElement("span");
+    }
+
+    wrapper.innerHTML = " " + choice.body;
     // create radio button
     let input = document.createElement("input");
     input.setAttribute("type", "radio");
     input.setAttribute("name", "choice");
     input.setAttribute("value", index);
     label.appendChild(input);
-    label.appendChild(span);
+    label.appendChild(wrapper);
     document.getElementById("choices").appendChild(label);
   });
 
@@ -56,15 +71,48 @@ function showQuestion(index, data) {
   document.getElementById("check-answer").blur();
 }
 
-function checkAnswer() {
+function checkAnswer(questionNumber, data) {
   var choice = document.querySelector('input[name="choice"]:checked');
+  let el = document.getElementById("response");
   if (!choice) {
-    console.log("please choose an answer");
+    el.innerHTML = "Please select an answer.";
+    el.setAttribute("class", "incorrect");
   } else if (choice && data[questionNumber].choices[choice.value].correct) {
-    console.log("Correct");
+    el.innerHTML = "Correct!";
+    el.setAttribute("class", "correct");
+    let question = document.getElementById("question");
+    question.style.display = "none";
+    let choices = document.getElementById("choices");
+    choices.style.display = "none";
+    let button = document.getElementById("check-answer");
+    button.innerHTML = "Review";
+    button.blur();
+    button.onclick = () => resetQuestion(questionNumber, data);
   } else {
-    console.log("That's not correct");
+    el.innerHTML = "That's not correct";
+    el.setAttribute("class", "incorrect");
   }
 }
 
-showQuestion(question, data);
+function setButton(questionNumber, data) {
+  let button = document.getElementById("check-answer");
+  button.innerHTML = "check answer";
+  button.blur();
+  button.onclick = () => checkAnswer(questionNumber, data);
+}
+
+function resetQuestion(questionNumber, data) {
+  let el = document.getElementById("response");
+  el.innerHTML = "";
+  let button = document.getElementById("check-answer");
+  button.innerHTML = "check answer";
+  button.blur();
+  button.onclick = checkAnswer;
+  let question = document.getElementById("question");
+  question.style.display = "block";
+  let choices = document.getElementById("choices");
+  choices.style.display = "block";
+}
+
+showQuestion(questionNumber(), data);
+setButton(questionNumber(), data);
